e0c8c0f7f2c8c2f6d25961881dc55bc63775a2c1
time2fishman <adam_h_martinez@hotmail.com>
2023-11-14 13:33:51 -0700

    required dotenv hoping PORT comes in from Render

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require('express')
 const bcrypt = require('bcrypt-nodejs')
 const cors = require('cors')
 const knex = require('knex')
+const dotenv = require('dotenv')
+dotenv.config()
 
 const register = require('./controllers/register')
 const signin = require('./controllers/signin')
 const profile = require('./controllers/profile')
 const image = require('./controllers/image')
+const PORT = process.env.PORT || 3001
 
 const db = knex({
   client: 'pg',
@@ -30,6 +33,6 @@ app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) })
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleAPICall(req, res) })
 
-app.listen(process.env.PORT || 3001, () => {
+app.listen(PORT || 3001, () => {
   console.log(`App is running on port: ${PORT}`);
 })
