8ec58139552e8ed4c6e520d1325132a08ca920dc
time2fishman <adam_h_martinez@hotmail.com>
2023-11-14 13:51:28 -0700

    change register error message

--- controllers/register.js
+++ controllers/register.js
@@ -27,7 +27,7 @@ const handleRegister = (req, res, db, bcrypt) => {
       .then(trx.commit)
       .catch(trx.rollback)
   })
-    .catch(err => res.status(400).json('Email address already in use. Try logging in instead.'))
+    .catch(err => res.status(400).json('Unable to register'))
 }
 
 module.exports = {
